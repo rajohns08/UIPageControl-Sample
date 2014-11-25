eda7766d7a089db464c8cf611f2a4619dfd115d8
rajohns <rajohns@bcbsms.com>
2014-11-25 08:26:55 -0600

    Initial Commit

---
+++ PageController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PageController
+//
+//  Created by Adam Johns on 11/25/14.
+//  Copyright (c) 2014 Adam Johns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PageController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PageController
+//
+//  Created by Adam Johns on 11/25/14.
+//  Copyright (c) 2014 Adam Johns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
